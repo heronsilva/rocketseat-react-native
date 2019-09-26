09062d8b9df131677c137bcdf9fd83500a77336c
Heron Silva <heron.code@outlook.com>
2019-09-26 12:39:24 -0300

    Initial commit

---
+++ src/pages/main.js
@@ -0,0 +1,122 @@
+import React, { Component } from 'react';
+import api from '../services/api';
+
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  StyleSheet,
+} from 'react-native';
+
+export default class Main extends Component {
+  state = {
+    productInfo: {},
+    docs: [],
+    page: 1,
+  };
+
+  static navigationOptions = {
+    title: 'JSHunt',
+  };
+
+  componentDidMount() {
+    this.loadProducts();
+  }
+
+  loadProducts = async (page = 1) => {
+    const response = await api.get(`/products?page=${page}`);
+    const { docs, ...productInfo } = response.data;
+
+    this.setState({ docs: [...this.state.docs, ...docs], productInfo, page });
+  };
+
+  loadMore = () => {
+    const { page, productInfo } = this.state;
+
+    if (page === productInfo.pages) { return; }
+
+    const pageNumber = page + 1;
+
+    this.loadProducts(pageNumber);
+  };
+
+  renderItem = ({ item }) => (
+    <View style={style.productContainer}>
+      <Text style={style.productTitle}>{item.title}</Text>
+      <Text style={style.productDescription}>{item.description}</Text>
+      <TouchableOpacity
+        style={style.productButton}
+        onPress={() => {
+          this.props.navigation.navigate('Product', { product: item });
+        }}>
+        <Text style={style.productButtonText}>Acessar</Text>
+      </TouchableOpacity>
+    </View>
+  );
+
+  render() {
+    return (
+      <View style={style.container}>
+        <FlatList
+          contentContainerStyle={style.list}
+          data={this.state.docs}
+          keyExtractor={item => item._id}
+          renderItem={this.renderItem}
+          onEndReached={this.loadMore()}
+          onEndReachedThreshold={0.1}
+        />
+      </View>
+    );
+  }
+}
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FAFAFA',
+  },
+
+  list: {
+    padding: 20,
+  },
+
+  productContainer: {
+    backgroundColor: '#FFF',
+    borderWidth: 1,
+    borderColor: '#DDD',
+    borderRadius: 5,
+    padding: 20,
+    marginBottom: 20,
+  },
+
+  productTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+
+  productDescription: {
+    fontSize: 16,
+    color: '#999',
+    marginTop: 5,
+    lineHeight: 24,
+  },
+
+  productButton: {
+    height: 42,
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: '#DA552F',
+    backgroundColor: 'transparent',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 10,
+  },
+
+  productButtonText: {
+    fontSize: 16,
+    color: '#DA552F',
+    fontWeight: 'bold',
+  },
+});
